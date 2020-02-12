d22fd7585716be57f0717e0c42cb45afb9f4567f
Dylan ALIZON <dylan.alizon@gmail.com>
2020-02-12 20:15:30 +0100

    Installation theme

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var Encore = require('@symfony/webpack-encore');
+
+// Manually configure the runtime environment if not already configured yet by the "encore" command.
+// It's useful when you use tools that rely on webpack.config.js file.
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+Encore
+    .setOutputPath('public/build/')
+    .setPublicPath('/build')
+    .addEntry('app', './assets/js/app.js')
+    .splitEntryChunks()
+    .enableSingleRuntimeChunk()
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = 3;
+    })
+    .enableSassLoader()
+    .autoProvidejQuery()
+    .copyFiles({
+        from: './assets/images',
+        to: 'images/[path][name].[ext]'
+      }
+    )
+;
+
+module.exports = Encore.getWebpackConfig();
