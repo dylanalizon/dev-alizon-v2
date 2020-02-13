6c34a9b627c3684188aa4e36ea71fcbaab666f74
Dylan ALIZON <dylan.alizon@gmail.com>
2020-02-13 18:08:56 +0100

    Modifications theme + creation navbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,11 @@
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import '@fortawesome/fontawesome-free/js/all.min';
 import '../css/app.scss';
-import './browser.min';
-import breakpoints from './breakpoints.min';
-import './util';
 
 (function($) {
 
-  var	$window = $(window),
-    $banner = $('#banner'),
+  let	$window = $(window),
     $body = $('body');
-
-  // Breakpoints.
-  breakpoints({
-    default:   ['1681px',   null       ],
-    xlarge:    ['1281px',   '1680px'   ],
-    large:     ['981px',    '1280px'   ],
-    medium:    ['737px',    '980px'    ],
-    small:     ['481px',    '736px'    ],
-    xsmall:    ['361px',    '480px'    ],
-    xxsmall:   [null,       '360px'    ]
-  });
 
   // Play initial animations on page load.
   $window.on('load', function() {
@@ -27,19 +14,19 @@ import './util';
     }, 100);
   });
 
-  // Menu.
-  $('#menu')
-    .append('<a href="#menu" class="close"></a>')
-    .appendTo($body)
-    .panel({
-      target: $body,
-      visibleClass: 'is-menu-visible',
-      delay: 500,
-      hideOnClick: true,
-      hideOnSwipe: true,
-      resetScroll: true,
-      resetForms: true,
-      side: 'right'
-    });
+
+  $window.scroll(function() {
+    if ($(document).scrollTop() > 50) {
+      $('.header__nav').addClass('header__affix');
+    } else {
+      $('.header__nav').removeClass('header__affix');
+    }
+  });
+
+  $('.header__navTrigger').click(function () {
+    $(this).toggleClass('active');
+    $("#header__mainListDiv").toggleClass("header__show_list");
+    $("#header__mainListDiv").fadeIn();
+  });
 
 })(jQuery);
