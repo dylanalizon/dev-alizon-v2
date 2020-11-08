aa732d45eb4d648c6fefd84fdd8efb97c96fde89
Dylan ALIZON <dylan.alizon@gmail.com>
2020-11-08 03:29:19 +0100

    JS: remove jQuery

--- assets/js/turbolinks.js
+++ assets/js/turbolinks.js
@@ -3,29 +3,33 @@ import Turbolinks from 'turbolinks';
 Turbolinks.start();
 
 document.addEventListener("turbolinks:load", function() {
-  let	$window = $(window);
 
   // Play initial animations on page load.
-  setTimeout(function() {
-    $('body').removeClass('is-preload');
+  setTimeout(() => {
+    document.body.classList.remove('is-preload');
   }, 100);
 
   // On scroll => change navbar
-  $window.scroll(function() {
-    if ($(document).scrollTop() > 50) {
-      $('.header__nav').addClass('header__affix');
+  document.addEventListener('scroll', () => {
+    const headerNav = document.querySelector('.header__nav');
+    if (window.scrollY > 50) {
+      headerNav.classList.add('header__affix');
     } else {
-      $('.header__nav').removeClass('header__affix');
+      headerNav.classList.remove('header__affix');
     }
   });
 
-  // on Hambergur menu click
-  $('.header__navTrigger').click(function () {
-    $(this).toggleClass('active');
-    let $headerMainListDiv = $("#header__mainListDiv");
-    $headerMainListDiv.toggleClass("header__show_list");
-    $headerMainListDiv.fadeIn();
-  });
+  // on Hamburger menu click
+  const hamburgerMenu = document.querySelector('.header__navTrigger');
+  if (hamburgerMenu) {
+    hamburgerMenu.addEventListener('click', () => {
+      hamburgerMenu.classList.toggle('active');
+      const headerMainListDiv = document.getElementById('header__mainListDiv');
+      if (headerMainListDiv) {
+        headerMainListDiv.classList.toggle("header__show_list");
+      }
+    });
+  }
 });
 
 Turbolinks.scroll = {};
